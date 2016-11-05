253e1b7c50f15f7acc66686e19a81f2aedb5ee94
Haw Jeh <hawjeh@gmail.com>
2016-11-06 00:22:09 +0800

    Update about

--- controllers/weather/sg-weather-config.js
+++ controllers/weather/sg-weather-config.js
@@ -1,4 +1,4 @@
-const token = "";
+const token = "qkdkexfqLZld1fdtopesj9TAIANVNV8x";
 const url = "https://api.data.gov.sg/v1/environment/24-hour-weather-forecast";
 const daysUrl = "https://api.data.gov.sg/v1/environment/4-day-weather-forecast";
 
